8bee44c796948e223c61666b9e7479ca3bc8552b
emre-olgun <emreolgun140@gmail.com>
2023-12-09 12:37:38 +0000

    image build folder configured

--- jenkins/image-build.sh
+++ jenkins/image-build.sh
@@ -1,3 +1,3 @@
-docker build --force-rm -t "$(IMAGE_TAG_REACT)" "${WORKSPACE}/k8s/client/Dockerfile"
-docker build --force-rm -t "$(IMAGE_TAG_NODE)" "${WORKSPACE}/k8s/server/Dockerfile"
-docker build --force-rm -t "$(IMAGE_TAG_POSTGRES)" "${WORKSPACE}/k8s/database/Dockerfile"
+docker build --force-rm -t "$(IMAGE_TAG_REACT)" "${WORKSPACE}/k8s/client"
+docker build --force-rm -t "$(IMAGE_TAG_NODE)" "${WORKSPACE}/k8s/server"
+docker build --force-rm -t "$(IMAGE_TAG_POSTGRES)" "${WORKSPACE}/k8s/database"
